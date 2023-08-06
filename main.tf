02d5b2971737779df9bdfd47bb11eb1ae4d48b8e
xneyder <xneyder@gmail.com>
2023-08-06 01:33:00 -0400

    Add files

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_docdb_cluster" "default" {
   kms_key_id                      = var.create_kms_key ? aws_kms_key.default[0].arn : null
   port                            = var.db_port
   snapshot_identifier             = var.snapshot_identifier
-  vpc_security_group_ids          = [join("", var.create_security_group ? aws_security_group.default[0].id : data.aws_security_group.default[0].id)]
+  vpc_security_group_ids          = [join("", var.create_security_group ? aws_security_group.default[*].id : data.aws_security_group.default[*].id)]
   db_subnet_group_name            = var.db_subnet_group_name
   db_cluster_parameter_group_name = var.db_cluster_parameter_group_name
   engine                          = var.engine
