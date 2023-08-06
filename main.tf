509090deeef7e809493495c1774ae9f0fa0447f8
xneyder <xneyder@gmail.com>
2023-08-06 01:31:12 -0400

    Add files

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ resource "aws_security_group_rule" "default" {
   to_port           = try(each.value.to_port, -1)
   protocol          = each.value.protocol
   cidr_blocks       = each.value.cidr_blocks
-  security_group_id = try(aws_security_group.default[0].id, data.aws_security_group.default[*].id)
+  security_group_id = var.create_security_group ? aws_security_group.default[0].id : data.aws_security_group.default[0].id
+
 }
 
 resource "random_password" "password" {
@@ -45,7 +46,7 @@ resource "aws_docdb_cluster" "default" {
   kms_key_id                      = var.create_kms_key ? aws_kms_key.default[0].arn : null
   port                            = var.db_port
   snapshot_identifier             = var.snapshot_identifier
-  vpc_security_group_ids          = [join("", try(aws_security_group.default[*].id, data.aws_security_group.default[*].id))]
+  vpc_security_group_ids          = [join("", var.create_security_group ? aws_security_group.default[0].id : data.aws_security_group.default[0].id)]
   db_subnet_group_name            = var.db_subnet_group_name
   db_cluster_parameter_group_name = var.db_cluster_parameter_group_name
   engine                          = var.engine
